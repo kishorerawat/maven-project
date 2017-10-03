38e42e079cafc56db202ecf27faa95ec95d0a353
kishorerawat <kishore.rawat@gmail.com>
2017-10-04 02:53:43 +0530

    Added JavaDoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,9 @@ public class Greeter {
 
   }
 
+  /**
+   * This is greet method to greet someone
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
