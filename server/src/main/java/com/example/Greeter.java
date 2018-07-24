39383077ab35059c0e4c4086168bb5545a8be45e
Ubuntu <ubuntu@ip-172-31-3-184.ap-southeast-1.compute.internal>
2018-07-24 14:49:34 +0000

    Corrected warnings reported by Checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,9 @@ public class Greeter {
   }
 
   /**
-   * This is greet method to greet someone
+   * This is greet method to greet someone.
+   * @param someone the name of a person
+   * @return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
